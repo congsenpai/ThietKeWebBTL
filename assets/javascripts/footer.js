0b87d98efbd1bc92d01513e94d079a9d76b03412
congsenpai <congcoihv5@gmail.com>
2024-05-25 18:59:29 +0700

    gg

--- assets/javascripts/footer.js
+++ assets/javascripts/footer.js
@@ -1,6 +1,6 @@
 function fetchFooter() {
     // Sử dụng fetch để lấy nội dung của footer.html
-    fetch('../components/footer.html')
+    fetch('/ThietKeWebBTL/components/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
