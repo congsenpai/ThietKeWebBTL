3438e3dc8648279c1e0de6557ea31e1576484c2d
congsenpai <congcoihv5@gmail.com>
2024-06-14 07:57:24 +0700

    add jquery

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,3 +10,22 @@ function fetchhotProduct() {
 document.addEventListener('DOMContentLoaded', function() {
     fetchhotProduct();
 });
+// navbar.js
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar__big').innerHTML = data;
+
+            // Thực hiện thao tác với các phần tử sau khi đã chèn xong
+            const navbarItems = document.getElementsByClassName("navbar__item");
+            Array.from(navbarItems).forEach(item => {
+                item.addEventListener('mouseover', () => {
+                    modalOn();
+                });
+                item.addEventListener('mouseout', () => {
+                    modalOff();
+                });
+            });
+        });
+});
